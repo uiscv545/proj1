b3a814a3813c99ad66ffac8f777a78afc8696612
ProMaxService <pv731957@gmail.com>
2026-05-06 02:09:46 +0330

    Update relay.js

--- netlify/edge-functions/relay.js
+++ netlify/edge-functions/relay.js
@@ -7,7 +7,8 @@ export const config = {
   maxDuration: 60,
 };
 
-const TARGET_BASE = "https://inb.promaxvpn.site:8096";
+const TARGET_BASE = Netlify.env.get("TARGET_BASE") || "https://inb.promaxvpn.site:8096";
+
 
 const STRIP_HEADERS = new Set([
   "host",
